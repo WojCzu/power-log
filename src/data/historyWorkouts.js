c47953948b6981bad70b28033c6a775e385cbd89
WojCzu <wojciech.czuma@gmail.com>
2021-09-19 21:31:44 +0200

    add mockup data

---
+++ src/data/historyWorkouts.js
@@ -0,0 +1,350 @@
+export const historyWorkouts = [
+  {
+    id: 1,
+    date: '13.09.2021',
+    startingTime: '16:32',
+    endingTime: '18:31',
+    name: 'Lower Hyper',
+    mood: 'bad',
+    exercises: [
+      {
+        name: 'Low bar squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 150, volume: 6 },
+          { weight: 155, volume: 6 },
+          { weight: 160, volume: 6 },
+        ],
+      },
+      {
+        name: 'Bulgarian split squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+        ],
+      },
+      {
+        name: 'Plank',
+        measurement: 'seconds',
+        sets: [
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 2,
+    date: '12.09.2021',
+    startingTime: '16:32',
+    endingTime: '17:31',
+    name: 'Upper hyper',
+    mood: 'good',
+    exercises: [
+      {
+        name: 'Bench press',
+        measurement: 'reps',
+        sets: [
+          { weight: 75, volume: 8 },
+          { weight: 77.5, volume: 8 },
+          { weight: 80, volume: 7 },
+        ],
+      },
+      {
+        name: 'Pull up',
+        measurement: 'reps',
+        sets: [
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+        ],
+      },
+      {
+        name: 'Hammer curl',
+        measurement: 'reps',
+        sets: [
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 9 },
+        ],
+      },
+      {
+        name: 'Cable pushdown',
+        measurement: 'reps',
+        sets: [
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 11 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 3,
+    date: '11.09.2021',
+    startingTime: '16:32',
+    endingTime: '18:31',
+    name: 'Lower Hyper',
+    mood: 'bad',
+    exercises: [
+      {
+        name: 'Low bar squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 150, volume: 6 },
+          { weight: 155, volume: 6 },
+          { weight: 160, volume: 6 },
+        ],
+      },
+      {
+        name: 'Bulgarian split squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+        ],
+      },
+      {
+        name: 'Plank',
+        measurement: 'seconds',
+        sets: [
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 4,
+    date: '10.09.2021',
+    startingTime: '16:32',
+    endingTime: '17:31',
+    name: 'Upper hyper',
+    mood: 'good',
+    exercises: [
+      {
+        name: 'Bench press',
+        measurement: 'reps',
+        sets: [
+          { weight: 75, volume: 8 },
+          { weight: 77.5, volume: 8 },
+          { weight: 80, volume: 7 },
+        ],
+      },
+      {
+        name: 'Pull up',
+        measurement: 'reps',
+        sets: [
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+        ],
+      },
+      {
+        name: 'Hammer curl',
+        measurement: 'reps',
+        sets: [
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 9 },
+        ],
+      },
+      {
+        name: 'Cable pushdown',
+        measurement: 'reps',
+        sets: [
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 11 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 5,
+    date: '09.09.2021',
+    startingTime: '16:32',
+    endingTime: '18:31',
+    name: 'Lower Hyper',
+    mood: 'bad',
+    exercises: [
+      {
+        name: 'Low bar squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 150, volume: 6 },
+          { weight: 155, volume: 6 },
+          { weight: 160, volume: 6 },
+        ],
+      },
+      {
+        name: 'Bulgarian split squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+        ],
+      },
+      {
+        name: 'Plank',
+        measurement: 'seconds',
+        sets: [
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 6,
+    date: '08.09.2021',
+    startingTime: '16:32',
+    endingTime: '17:31',
+    name: 'Upper hyper',
+    mood: 'good',
+    exercises: [
+      {
+        name: 'Bench press',
+        measurement: 'reps',
+        sets: [
+          { weight: 75, volume: 8 },
+          { weight: 77.5, volume: 8 },
+          { weight: 80, volume: 7 },
+        ],
+      },
+      {
+        name: 'Pull up',
+        measurement: 'reps',
+        sets: [
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+        ],
+      },
+      {
+        name: 'Hammer curl',
+        measurement: 'reps',
+        sets: [
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 9 },
+        ],
+      },
+      {
+        name: 'Cable pushdown',
+        measurement: 'reps',
+        sets: [
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 11 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 7,
+    date: '07.09.2021',
+    startingTime: '16:32',
+    endingTime: '18:31',
+    name: 'Lower Hyper',
+    mood: 'bad',
+    exercises: [
+      {
+        name: 'Low bar squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 150, volume: 6 },
+          { weight: 155, volume: 6 },
+          { weight: 160, volume: 6 },
+        ],
+      },
+      {
+        name: 'Bulgarian split squat',
+        measurement: 'reps',
+        sets: [
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+          { weight: 12, volume: 12 },
+        ],
+      },
+      {
+        name: 'Plank',
+        measurement: 'seconds',
+        sets: [
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+          { weight: 0, volume: 60 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+  {
+    id: 8,
+    date: '06.09.2021',
+    startingTime: '16:32',
+    endingTime: '17:31',
+    name: 'Upper hyper',
+    mood: 'good',
+    exercises: [
+      {
+        name: 'Bench press',
+        measurement: 'reps',
+        sets: [
+          { weight: 75, volume: 8 },
+          { weight: 77.5, volume: 8 },
+          { weight: 80, volume: 7 },
+        ],
+      },
+      {
+        name: 'Pull up',
+        measurement: 'reps',
+        sets: [
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+          { weight: 0, volume: 12 },
+        ],
+      },
+      {
+        name: 'Hammer curl',
+        measurement: 'reps',
+        sets: [
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 12 },
+          { weight: 10, volume: 9 },
+        ],
+      },
+      {
+        name: 'Cable pushdown',
+        measurement: 'reps',
+        sets: [
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 10 },
+          { weight: 15, volume: 11 },
+        ],
+      },
+    ],
+    notes:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem nisi voluptatem.',
+  },
+];
