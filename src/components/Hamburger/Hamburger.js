4ac0279f1f8ef894cc16a0c6d262fc095192b75b
WojCzu <wojciech.czuma@gmail.com>
2021-09-19 21:12:56 +0200

    Add screen reader text to burger button

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { SrOnly } from 'components/SrOnly/SrOnly';
 import { Wrapper, Box } from './Hamburger.styles';
 
 const Hamburger = ({ handleClick, isOpen, hamburgerRef }) => {
   return (
     <Wrapper onClick={handleClick} ref={hamburgerRef}>
+      <SrOnly>{isOpen ? 'Close menu' : 'Open menu'}</SrOnly>
       <Box isOpen={isOpen}>
         <span></span>
       </Box>
