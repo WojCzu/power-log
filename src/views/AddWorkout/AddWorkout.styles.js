ca25867ea301a97101bbc95cf943293aead6f8d3
WojCzu <wojciech.czuma@gmail.com>
2021-10-03 16:30:58 +0200

    fix AddWorkout padding and scroll

--- src/views/AddWorkout/AddWorkout.styles.js
+++ src/views/AddWorkout/AddWorkout.styles.js
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.form`
   margin: 0 auto;
   padding: 1px 20px 20px;
-  height: 100%;
   width: clamp(280px, 100%, 860px);
-  overflow-y: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
